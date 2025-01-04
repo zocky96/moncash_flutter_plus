6d3ca4d40c72221827e75b074f705022219bade7
zocky96 <zocky58@gmail.com>
2025-01-04 13:55:37 -0600

    ajoute rere

--- lib/src/moncash.dart
+++ lib/src/moncash.dart
@@ -13,127 +13,109 @@ class MonCashPayment extends StatefulWidget {
   final String clientSecret;
   final Widget? loadingWidget;
   final bool isStaging;
-  const MonCashPayment(
-      {required this.amount,
-      this.orderId,
-      required this.clientId,
-      required this.clientSecret,
-      this.loadingWidget,
-      this.isStaging = false,
-      Key? key})
-      : super(key: key);
+  const MonCashPayment({
+    required this.amount,
+    this.orderId,
+    required this.clientId,
+    required this.clientSecret,
+    this.loadingWidget,
+    this.isStaging = false,
+    Key? key,
+  }) : super(key: key);
 
   @override
   _MonCashPaymentState createState() => _MonCashPaymentState();
 }
 
 class _MonCashPaymentState extends State<MonCashPayment> {
-  late final WebViewController _webViewController;
-  late final String? paymentUrl = "";
-  var theurl="";
-  String myUrl = "";
-  bool _isLoading = true;
-  bool _hasError = false;
   late MonCash monCash;
+  late final WebViewController _webViewController;
   bool isLoading = true;
   String orderId = DateTime.now().millisecondsSinceEpoch.toString();
+  String? paymentUrl;
+
   @override
   void initState() {
     super.initState();
-    monCash = MonCash(clientId: widget.clientId, clientSecret: widget.clientSecret, staging: widget.isStaging);
+
+    monCash = MonCash(
+      clientId: widget.clientId,
+      clientSecret: widget.clientSecret,
+      staging: widget.isStaging,
+    );
+
     if (widget.orderId != null) {
       orderId = widget.orderId!;
     }
+
     monCash.getWebviewUrl(amount: widget.amount.toString(), orderId: orderId).then((value) {
-
       if (value != null) {
-        _loadUrl() async{
-          setState(() => paymentUrl = "${value}");
-        }
-      } else {
-        Navigator.pop(
-            context,
-            PaymentResponse(
-                status: paymentStatus.failed, message: "Error in generating token, Please try again later.."));
-      }
-    });
+        setState(() => paymentUrl = value);
 
-    _loadUrl() async{
-      setState(() => myUrl = "${paymentUrl}");
-    }
-    _webViewController = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onPageStarted: (String url) {
-            setState(() {
-              _isLoading = true;
-              _hasError = false;
-            });
-          },
-          onPageFinished: (String url) {
-            setState(() {
-              isLoading = false;
-            });
+        // Initialiser le WebViewController avec l'URL de paiement
+        _webViewController = WebViewController()
+          ..loadRequest(Uri.parse(paymentUrl!))
+          ..setJavaScriptMode(JavaScriptMode.unrestricted)
+          ..setNavigationDelegate(
+            NavigationDelegate(
+              onPageFinished: (url) {
+                setState(() => isLoading = false);
+                log(url);
 
-            log(url);
-            //getting transanction id from moncash url
-            if (url.contains('transactionId')) {
-              final transactionId = url.split('transactionId=')[1];
-              log('transactionId: $transactionId');
-              //   monCash.retrieveTransactionPayment(transactionId);
-              //  displaySnackBar("Payment Successfull $transactionId", context);
-              Navigator.pop(
-                  context,
-                  PaymentResponse(
+                if (url.contains('transactionId')) {
+                  final transactionId = Uri.parse(url).queryParameters['transactionId'];
+                  log('transactionId: $transactionId');
+                  Navigator.pop(
+                    context,
+                    PaymentResponse(
                       transanctionId: transactionId,
                       orderId: orderId,
                       status: paymentStatus.success,
-                      message: "Payment Successfull $transactionId"));
-            }
-            if (url.contains('error')) {
-              final errorData = url.split('error=');
-              String error = errorData.length > 1 ? errorData[1] : 'Error, Please Try Again Later';
-              log('error: $error');
-              Navigator.pop(
-                  context, PaymentResponse(status: paymentStatus.failed, message: error, orderId: orderId));
-            }
-            setState(() {
-              _isLoading = false;
-            });
-          },
-          onWebResourceError: (WebResourceError error) {
-            setState(() {
-              _isLoading = false;
-              _hasError = true;
-            });
-          },
-        ),
-      )
-      ..loadRequest(Uri.parse("${paymentUrl}"),
-      );
-    print(paymentUrl);
-
+                      message: "Payment Successful $transactionId",
+                    ),
+                  );
+                } else if (url.contains('error')) {
+                  final error = Uri.parse(url).queryParameters['error'] ?? 'Error, Please Try Again Later';
+                  log('error: $error');
+                  Navigator.pop(
+                    context,
+                    PaymentResponse(
+                      status: paymentStatus.failed,
+                      message: error,
+                      orderId: orderId,
+                    ),
+                  );
+                }
+              },
+            ),
+          );
+      } else {
+        Navigator.pop(
+          context,
+          PaymentResponse(
+            status: paymentStatus.failed,
+            message: "Error in generating token, Please try again later.",
+          ),
+        );
+      }
+    });
   }
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('${myUrl}'),
-      ),
       body: Stack(
         children: [
-
           if (paymentUrl != null)
             WebViewWidget(controller: _webViewController),
           if (paymentUrl == null || isLoading)
             Container(
-                color: Colors.white,
-                height: MediaQuery.of(context).size.height,
-                child: Center(child: widget.loadingWidget ?? const CircularProgressIndicator()))
+              color: Colors.white,
+              height: MediaQuery.of(context).size.height,
+              child: Center(
+                child: widget.loadingWidget ?? const CircularProgressIndicator(),
+              ),
+            ),
         ],
       ),
     );
