309c4aa9295b7be97f67d91d1ff4bf34357fb998
zocky96 <zocky58@gmail.com>
2025-01-04 23:29:30 -0600

    ajoute rere

--- lib/src/moncash.dart
+++ lib/src/moncash.dart
@@ -56,10 +56,10 @@ class _MonCashPaymentState extends State<MonCashPayment> {
       } else {
         Navigator.pop(
           context,
-          PaymentResponse(
-            status: paymentStatus.failed,
-            message: "Error in generating token, Please try again later.",
-          ),
+          // PaymentResponse(
+          //   status: paymentStatus.failed,
+          //   message: "Error in generating token, Please try again later.",
+          // ),
         );
       }
     });
@@ -71,13 +71,13 @@ class _MonCashPaymentState extends State<MonCashPayment> {
       onWillPop: () async {
         Navigator.pop(
           context,
-        
+
           //
-          PaymentResponse(
-            status: paymentStatus.failed,
-            message: "Payment cancelled by user.",
-            orderId: orderId,
-          ),
+          // PaymentResponse(
+          //   status: paymentStatus.failed,
+          //   message: "Payment cancelled by user.",
+          //   orderId: orderId,
+          // ),
 
         );
         return Future.value(false); // Empêche la fermeture automatique
@@ -116,11 +116,11 @@ class _MonCashPaymentState extends State<MonCashPayment> {
                     log('error: $error');
                     Navigator.pop(
                       context,
-                      PaymentResponse(
-                        status: paymentStatus.failed,
-                        message: error,
-                        orderId: orderId,
-                      ),
+                      // PaymentResponse(
+                      //   status: paymentStatus.failed,
+                      //   message: error,
+                      //   orderId: orderId,
+                      // ),
                     );
                   }
                 },
