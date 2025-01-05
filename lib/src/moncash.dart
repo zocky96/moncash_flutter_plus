8a572055885a1fe74bb1f328d8ba2e2b0dabbabb
zocky96 <zocky58@gmail.com>
2025-01-04 22:52:37 -0600

    ajoute rere

--- lib/src/moncash.dart
+++ lib/src/moncash.dart
@@ -67,58 +67,71 @@ class _MonCashPaymentState extends State<MonCashPayment> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          if (paymentUrl != null)
-            InAppWebView(
-              key: webViewKey,
-              initialUrlRequest: URLRequest(url: WebUri(paymentUrl!)),
-              initialOptions: InAppWebViewGroupOptions(
-                crossPlatform: InAppWebViewOptions(javaScriptEnabled: true),
-              ),
-              onWebViewCreated: (controller) {
-                webViewController = controller;
-              },
-              onLoadStop: (controller, url) {
-                setState(() => isLoading = false);
-                log(url.toString());
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.pop(
+          context,
+          PaymentResponse(
+            status: paymentStatus.cancelled,
+            message: "Payment cancelled by user.",
+            orderId: orderId,
+          ),
+        );
+        return Future.value(false); // Empêche la fermeture automatique
+      },
+      child: Scaffold(
+        body: Stack(
+          children: [
+            if (paymentUrl != null)
+              InAppWebView(
+                key: webViewKey,
+                initialUrlRequest: URLRequest(url: WebUri(paymentUrl!)),
+                initialOptions: InAppWebViewGroupOptions(
+                  crossPlatform: InAppWebViewOptions(javaScriptEnabled: true),
+                ),
+                onWebViewCreated: (controller) {
+                  webViewController = controller;
+                },
+                onLoadStop: (controller, url) {
+                  setState(() => isLoading = false);
+                  log(url.toString());
 
-                if (url.toString().contains('transactionId')) {
-                  final transactionId = Uri.parse(url.toString()).queryParameters['transactionId'];
-                  log('transactionId: $transactionId');
-                  Navigator.pop(
-                    context,
-                    PaymentResponse(
-                      transanctionId: transactionId,
-                      orderId: orderId,
-                      status: paymentStatus.success,
-                      message: "Payment Successful $transactionId",
-                    ),
-                  );
-                } else if (url.toString().contains('error')) {
-                  final error = Uri.parse(url.toString()).queryParameters['error'] ?? 'Error, Please Try Again Later';
-                  log('error: $error');
-                  Navigator.pop(
-                    context,
-                    PaymentResponse(
-                      status: paymentStatus.failed,
-                      message: error,
-                      orderId: orderId,
-                    ),
-                  );
-                }
-              },
-            ),
-          if (paymentUrl == null || isLoading)
-            Container(
-              color: Colors.white,
-              height: MediaQuery.of(context).size.height,
-              child: Center(
-                child: widget.loadingWidget ?? const CircularProgressIndicator(),
+                  if (url.toString().contains('transactionId')) {
+                    final transactionId = Uri.parse(url.toString()).queryParameters['transactionId'];
+                    log('transactionId: $transactionId');
+                    Navigator.pop(
+                      context,
+                      PaymentResponse(
+                        transanctionId: transactionId,
+                        orderId: orderId,
+                        status: paymentStatus.success,
+                        message: "Payment Successful $transactionId",
+                      ),
+                    );
+                  } else if (url.toString().contains('error')) {
+                    final error = Uri.parse(url.toString()).queryParameters['error'] ?? 'Error, Please Try Again Later';
+                    log('error: $error');
+                    Navigator.pop(
+                      context,
+                      PaymentResponse(
+                        status: paymentStatus.failed,
+                        message: error,
+                        orderId: orderId,
+                      ),
+                    );
+                  }
+                },
               ),
-            ),
-        ],
+            if (paymentUrl == null || isLoading)
+              Container(
+                color: Colors.white,
+                height: MediaQuery.of(context).size.height,
+                child: Center(
+                  child: widget.loadingWidget ?? const CircularProgressIndicator(),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
